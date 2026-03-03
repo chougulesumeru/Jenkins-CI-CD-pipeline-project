f3d01c440b767719bac4de87b6fb2b65d73afbf8
chougulesumeru <chougulesumeru19@gmail.com>
2026-03-03 11:52:12 +0530

    files modified

---
+++  main.tf
@@ -0,0 +1,23 @@
+#configure eks eith terraform 
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~>17.0"
+
+  cluster_name    = "jenkins-prject-cluster"
+  cluster_version = "1.21"
+  subnets         = ["subnet123", "subnet123"]
+  vpc_id          = "vpc-123456789"
+
+  node_groups = {
+    eks-node-group = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_type    = "t2.medium"
+    }
+  }
+}
+
+
+
